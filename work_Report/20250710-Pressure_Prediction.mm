3da7361fb1adf379c834aba75bd295c6bf919a1c
Bojun Zhang <1002129979@qq.com>
2025-08-01 14:50:46 +0800

    20250710-Pressure_Prediction.mm work_Report

---
+++ work_Report/20250710-Pressure_Prediction.mm
@@ -0,0 +1,263 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Pressure Prediction" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1752286973804" STYLE="oval" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt">
+<font SIZE="22"/>
+<hook NAME="MapStyle" zoom="1.1">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_icon_for_attributes="true" auto_compact_layout="false" show_tags="UNDER_NODES" associatedTemplateLocation="template:/Times_New_Romen.mm" show_note_icons="true" followedTemplateLocation="template:/standard-1.6.mm" followedMapLastTime="1748784985000" fit_to_viewport="false" show_icons="BESIDE_NODES" showTagCategories="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#000000" STYLE="fork" NUMBERED="false" FORMAT="STANDARD_FORMAT" TEXT_ALIGN="DEFAULT" TEXT_WRITING_DIRECTION="LEFT_TO_RIGHT" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt" CHILD_NODES_LAYOUT="AUTO" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#808080" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="Times New Roman" SIZE="10" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+<edge COLOR="#808080"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="30" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="黄伟老师 POF 2023" POSITION="bottom_or_right" ID="ID_1996202118" CREATED="1752284506689" MODIFIED="1752451945686" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt">
+<edge COLOR="#007c00"/>
+<richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Based on PointNet++
+    </p>
+  </body>
+</html></richcontent>
+<hook URI="file:///Users/zhangbojun/Documents/postGraduate/ML_Turbulent/思维导图/相关图片/Total%20flowchart.png" SIZE="0.33444816" NAME="ExternalObject"/>
+<node TEXT="Innovation" ID="ID_826102220" CREATED="1752289304960" MODIFIED="1752289560677" HGAP_QUANTITY="62.18898 pt" VSHIFT_QUANTITY="-69.16536 pt" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt">
+<node TEXT="Multiple input" ID="ID_1594606799" CREATED="1752287025650" MODIFIED="1752289204061" HGAP_QUANTITY="8.89764 pt" VSHIFT_QUANTITY="-72.56693 pt"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      One Fine dataset divided into multiple coarse subset.
+    </p>
+    <p>
+      And
+    </p>
+    <p>
+      assemble together in the end.
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="Multiple Sampling" ID="ID_1215240359" CREATED="1752287898497" MODIFIED="1752288325383"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      重复取样以减少粗糙数据带来的预测误差
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Outlier Filter" ID="ID_876797772" CREATED="1752288279132" MODIFIED="1752288706978"><richcontent TYPE="DETAILS">
+<text>\latex $[\mu_t-3\sigma_t, \mu_t+3\sigma_t]$</text></richcontent>
+</node>
+</node>
+<node TEXT="Cature Local Feature" ID="ID_399791909" CREATED="1752287675345" MODIFIED="1752289563294"><richcontent TYPE="DETAILS">
+<text>\latex $(x_i, n_k)$</text></richcontent>
+</node>
+</node>
+<node TEXT="Conclusion" ID="ID_1021567616" CREATED="1752289314774" MODIFIED="1752289567977" HGAP_QUANTITY="50.28347 pt" VSHIFT_QUANTITY="32.88189 pt">
+<node TEXT="升阻力系数的预测效果很好" ID="ID_1888353314" CREATED="1752289387119" MODIFIED="1752289481084"><richcontent TYPE="DETAILS">
+<text>\latex $C_d &gt; 0.9, C_l &gt; 0.9$</text></richcontent>
+</node>
+<node TEXT="压强系数的预测只有可视化后的图片" ID="ID_1608620384" CREATED="1752289388012" MODIFIED="1752289503091"/>
+</node>
+</node>
+<node TEXT="RegDGCNN" POSITION="top_or_left" ID="ID_1599342885" CREATED="1752284509523" MODIFIED="1752286937316" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt">
+<edge COLOR="#7c007c"/>
+<richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Based on PointNet and Graph CNN
+    </p>
+  </body>
+</html></richcontent>
+<font SIZE="14"/>
+<node TEXT="Innovation" ID="ID_344713575" CREATED="1752289522526" MODIFIED="1752289539395" VSHIFT_QUANTITY="-73.13386 pt">
+<node TEXT="Dense Point  Data" ID="ID_1081813376" CREATED="1752286766359" MODIFIED="1752286830118"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Just Input once
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Capture local geometirc structures" ID="ID_1205550771" CREATED="1752284626458" MODIFIED="1752285927584"><richcontent TYPE="DETAILS">
+<text>\latex $(x_i, x_i-x_j)$</text></richcontent>
+</node>
+<node TEXT="Dynamic Graph Update" ID="ID_1907624308" CREATED="1752285867875" MODIFIED="1752286242849"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      每进入一个卷积层，Graph就会发生改变，通过捕获局部特征来重新生成Graph
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Transform_Net" ID="ID_803505826" CREATED="1752286367391" MODIFIED="1752286571884"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      内嵌一个小型的神经网络，在每次更新Graph前进行
+    </p>
+    <p>
+      个人认为是一个优化代码
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="Conclusion" ID="ID_1050905236" CREATED="1752289529599" MODIFIED="1752289579603">
+<node TEXT="对单个车型的预测效果很好" ID="ID_46027959" CREATED="1752289582035" MODIFIED="1752289945528"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      \latex $R^2 \approx 0.9$
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="数据集为多种车型，预测效果一般" ID="ID_554380896" CREATED="1752289946648" MODIFIED="1752289977175"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      \latex $R^2 \approx 0.6$
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node TEXT="New method" POSITION="bottom_or_right" ID="ID_1429730308" CREATED="1752288899218" MODIFIED="1752289983458" HGAP_QUANTITY="-209.93701 pt" VSHIFT_QUANTITY="99.77953 pt">
+<edge COLOR="#ff00ff"/>
+<richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      在RegDGCNN模型的基础上进行修改
+    </p>
+  </body>
+</html></richcontent>
+<node ID="ID_1094166886" CREATED="1752288998896" MODIFIED="1752458462158"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Capture Nine Features 曲率<br/>&#xa0;如果是中心点，可以是小三角形面积
+    </p>
+  </body>
+</html>
+</richcontent>
+<richcontent TYPE="DETAILS">
+<text>\latex $(x_i, x_i-x_j, n_k)$</text></richcontent>
+</node>
+<node TEXT="Use Coarse Subset" ID="ID_714839722" CREATED="1752289067937" MODIFIED="1752289080719">
+<node TEXT="Multiple Sampling" ID="ID_1342272601" CREATED="1752289080724" MODIFIED="1752289088105"/>
+<node TEXT="Outlier Filter" ID="ID_1877715637" CREATED="1752289088644" MODIFIED="1752289104614"/>
+</node>
+<node TEXT="目前的疑惑" ID="ID_858176957" CREATED="1752456290271" MODIFIED="1752458465011" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt">
+<node TEXT="黄伟老师用2000个不同的飞行器模型进行训练&#xa;目前DrivDerNet只能提供六七种车型" ID="ID_97291945" CREATED="1752456306194" MODIFIED="1752458465010"/>
+</node>
+<node TEXT="最终目标" ID="ID_1331194771" CREATED="1752456372418" MODIFIED="1752456449137" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt">
+<node TEXT="R^2 在多种车型的训练模型下，训练结果要比RegDGCNN模型好一些" ID="ID_696817855" CREATED="1752456385632" MODIFIED="1752456449135" VSHIFT_QUANTITY="20.97638 pt"/>
+</node>
+</node>
+</node>
+</map>
