e59911a28e8976d50b84fbf85ff6f61b6f24f076
mae-zhangbj <1002129979@qq.com>
2025-08-15 12:18:46 +0800

    model/Transolver_Irregular_Mesh.py update

--- Pressure_Field/model/Transolver_Irregular_Mesh.py
+++ Pressure_Field/model/Transolver_Irregular_Mesh.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+import torch.nn.init as init
+import torch.nn.functional as F
 from timm.models.layers import trunc_normal_
 from model.Embedding import timestep_embedding
 import numpy as np
@@ -13,17 +15,17 @@ def knn(x, k):
     k-nearest neighbors algorithm.
 
     Args:
-        x: Input tensor of shape (batch_size, feature_dim, num_points)
+        x: Input tensor of shape (batch_size, num_points, feature_dim)
         k: Number of neighbors to consider
 
     Returns:
         Indices of k-nearest neighbors for each point
     """
-    inner = -2 * torch.matmul(x.transpose(2, 1), x)
-    xx = torch.sum(x ** 2, dim=1, keepdim=True)
-    pairwise_distance = -xx - inner - xx.transpose(2, 1)
+    inner = -2 * torch.matmul(x, x.transpose(2, 1))             # [batch_size, num_points, num_points]
+    xx = torch.sum(x ** 2, dim=2, keepdim=True)                 # [batch_size, num_points, 1]
+    pairwise_distance = -xx - inner - xx.transpose(2, 1)        # [batch_size, num_points, num_points]
 
-    idx = pairwise_distance.topk(k=k, dim=-1)[1]  # (batch_size, num_points, k)
+    idx = pairwise_distance.topk(k=k, dim=-1)[1]                # (batch_size, num_points, k)
     return idx
 
 
@@ -37,7 +39,7 @@ def get_graph_feature(x, k=20, idx=None, dim9=False):
     Then Enlarge it meeting the physical need
 
     Args:
-        x: Input tensor of shape (batch_size, feature_dim, num_points)
+        x: Input tensor of shape (batch_size, num_points, feature_dim)
         k: Number of neighbors to use for graph construction
         idx: Optional pre-computed nearest neighbor indices
         dim9: Whether to use additional dimensional features
@@ -46,42 +48,43 @@ def get_graph_feature(x, k=20, idx=None, dim9=False):
         Edge features for graph convolution
     """
     batch_size = x.size(0)
-    num_points = x.size(2)
-    x = x.view(batch_size, -1, num_points)
+    num_points = x.size(1)
+    x = x.view(batch_size, num_points, -1)
     if idx is None:
         idx = knn(x, k=k)  # (batch_size, num_points, k)
 
     device = x.device
-    idx_base = torch.arange(0, batch_size, device=device).view(-1, 1, 1) * num_points
+    idx_base = torch.arange(0, batch_size, device=device).view(-1, 1, 1) * num_points    # idx_base.shape = [2, 1, 1]
     idx = idx + idx_base
-    idx = idx.view(-1)
+    idx = idx.view(-1)                                                                   # [batch_size * num_points * k]
 
-    _, num_dims, _ = x.size()
-    x = x.transpose(2, 1).contiguous()  # (batch_size, num_points, num_dims)
+    _, _ , point_dims= x.size()
     feature = x.view(batch_size * num_points, -1)[idx, :]
-    feature = feature.view(batch_size, num_points, k, num_dims)
-    x = x.view(batch_size, num_points, 1, num_dims).repeat(1, 1, k, 1)
+    feature = feature.view(batch_size, num_points, k, point_dims)                        # [batch_size, num_points, k, point_dims]
+    x = x.view(batch_size, num_points, 1, point_dims).repeat(1, 1, k, 1)                 # [batch_size, num_points, k, point_dims]
 
     feature = torch.cat((feature - x, x), dim=3).permute(0, 3, 1, 2).contiguous()
-    return feature  # (batch_size, 2*num_dims, num_points, k)
+    return feature                                                                       # (batch_size, num_points, p_dim+d_dim , k)
 
 class Transform_Net(nn.Module):
-    def __init__(self, args):
+    #def __init__(self, args):
+    def __init__(self):
         super(Transform_Net, self).__init__()
-        self.args = args
-        self.k = 3
+        #self.args = args
+        ks=3
+       # self.k = 3
 
         self.bn1 = nn.BatchNorm2d(64)
         self.bn2 = nn.BatchNorm2d(128)
         self.bn3 = nn.BatchNorm1d(1024)
 
-        self.conv1 = nn.Sequential(nn.Conv2d(6, 64, kernel_size=1, bias=False),
+        self.conv1 = nn.Sequential(nn.Conv2d(6, 64, kernel_size=ks, bias=False),
                                    self.bn1,
                                    nn.LeakyReLU(negative_slope=0.2))
-        self.conv2 = nn.Sequential(nn.Conv2d(64, 128, kernel_size=1, bias=False),
+        self.conv2 = nn.Sequential(nn.Conv2d(64, 128, kernel_size=ks, bias=False),
                                    self.bn2,
                                    nn.LeakyReLU(negative_slope=0.2))
-        self.conv3 = nn.Sequential(nn.Conv1d(128, 1024, kernel_size=1, bias=False),
+        self.conv3 = nn.Sequential(nn.Conv1d(128, 1024, kernel_size=ks, bias=False),
                                    self.bn3,
                                    nn.LeakyReLU(negative_slope=0.2))
 
@@ -98,12 +101,11 @@ class Transform_Net(nn.Module):
         batch_size = x.size(0)
 
         x = self.conv1(x)                       # (batch_size, 3*2, num_points, k) -> (batch_size, 64, num_points, k)
-        x = self.conv2(x)                       # (batch_size, 64, num_points, k)  -> (batch_size, 128, num_points, k)
+        x = self.conv2(x)                       # (batch_size, 64, num_points, k) -> (batch_size, 128, num_points, k)
         x = x.max(dim=-1, keepdim=False)[0]     # (batch_size, 128, num_points, k) -> (batch_size, 128, num_points)
 
-        x = self.conv3(x)                       # (batch_size, 128, num_points)  -> (batch_size, 1024, num_points)
+        x = self.conv3(x)                       # (batch_size, 128, num_points) -> (batch_size, 1024, num_points)
         x = x.max(dim=-1, keepdim=False)[0]     # (batch_size, 1024, num_points) -> (batch_size, 1024)
-
         x = F.leaky_relu(self.bn4(self.linear1(x)), negative_slope=0.2)     # (batch_size, 1024) -> (batch_size, 512)
         x = F.leaky_relu(self.bn5(self.linear2(x)), negative_slope=0.2)     # (batch_size, 512) -> (batch_size, 256)
 
@@ -114,7 +116,8 @@ class Transform_Net(nn.Module):
 
 
 class MLP(nn.Module):
-    def __init__(self, n_input, n_hidden, n_output, n_layers=1, act='gelu', res=True):
+           # self.preprocess = MLP(space_dim, n_hidden * 2, n_hidden, n_layers=0, res=False, act=act)
+    def __init__(self, n_input, n_hidden, n_output, n_layers=1, act='gelu', res=True, k=40):
         super(MLP, self).__init__()
 
         if act in ACTIVATION.keys():
@@ -123,21 +126,48 @@ class MLP(nn.Module):
             raise NotImplementedError
         self.n_input = n_input
         self.n_hidden = n_hidden                    # n_hidden = 256
-        self.n_output = n_output
+        self.n_output = n_output                    # n_output = 128
         self.n_layers = n_layers
         self.res = res
-        self.linear_pre = nn.Sequential(nn.Linear(n_input, n_hidden), act())
-        self.linear_post = nn.Linear(n_hidden, n_output)
-        self.linears = nn.ModuleList([nn.Sequential(nn.Linear(n_hidden, n_hidden), act()) for _ in range(n_layers)])
+        self.transform_net = Transform_Net()
+        self.k = k
+        ks = 3
+       # self.linear_pre = nn.Sequential(nn.Linear(n_input, n_hidden), act())
+       # self.linear_post = nn.Linear(n_hidden, n_output)
+       # self.linears = nn.ModuleList([nn.Sequential(nn.Linear(n_hidden, n_hidden), act()) for _ in range(n_layers)])
+        self.bn1 = nn.BatchNorm2d(n_hidden)
+        self.bn2 = nn.BatchNorm2d(n_hidden)
+        self.bn3 = nn.BatchNorm2d(n_hidden)
+
+        self.conv1 = nn.Sequential(nn.Conv2d(n_input, n_hidden, kernel_size=ks, bias=False),
+                                   self.bn1,
+                                   nn.LeakyReLU(negative_slope=0.2))
+        self.conv2 = nn.Sequential(nn.Conv2d(n_hidden, n_hidden, kernel_size=ks, bias=False),
+                                   self.bn2,
+                                   nn.LeakyReLU(negative_slope=0.2))
+        self.conv3 = nn.Sequential(nn.Conv2d(n_hidden, n_output, kernel_size=ks, bias=False),
+                                   self.bn3,
+                                   nn.LeakyReLU(negative_slope=0.2))
 
     def forward(self, x):
-        x = self.linear_pre(x)                      # [B, num_points, point_dim] -> [B, num_points, 256]
-        for i in range(self.n_layers):
-            if self.res:
-                x = self.linears[i](x) + x
-            else:
-                x = self.linears[i](x)
-        x = self.linear_post(x)                     # [B, num_points, 256] -> [B, num_points, 128]
+        x0 = get_graph_feature(x, k=self.k)   # (batch_size, num_points, p_dim) -> (batch_size, p_dim+d_dim, num_points, k)
+        t = self.transform_net(x0)            # (batch_size, 3, 3)
+        x = torch.bmm(x, t)                   # (batch_size, num_points, 3) * (batch_size, 3, 3) -> (batch_size, num_points, 3)
+
+        x = get_graph_feature(x, k=self.k)    # (batch_size, num_points, p_dim) -> (batch_size, p_dim+d_dim, num_points, k)
+        x = self.conv1(x)                     # (batch_size, p_dim+d_dim, num_points, k) -> (batch_size, n_hidden, num_points, k)
+        x = self.conv2(x)                     # (batch_size, n_hidden, num_points, k) -> (batch_size, n_output, num_points, k)
+        x = x.max(dim=-1, keepdim=False)[0]   # (batch_size, n_output, num_points, k) -> (batch_size, n_output, num_points)
+
+#        x = self.linear_pre(x)                      # [B, num_points, point_dim] -> [B, num_points, 256]
+#        for i in range(self.n_layers):
+#            if self.res:
+#                x = self.linears[i](x) + x
+#            else:
+#                x = self.linears[i](x)
+#        x = self.linear_post(x)                     # [B, num_points, 256] -> [B, num_points, 128]
+        x = x.permute(0, 2, 1).contiguous()   # (batch_size, n_output, num_points) -> (batch_size, num_points, n_output)
+
         return x
 
 
@@ -161,14 +191,15 @@ class Transolver_block(nn.Module):
         self.Attn = Physics_Attention_Irregular_Mesh(hidden_dim, heads=num_heads, dim_head=hidden_dim // num_heads,
                                                      dropout=dropout, slice_num=slice_num)
         self.ln_2 = nn.LayerNorm(hidden_dim)
-        self.mlp = MLP(hidden_dim, hidden_dim * mlp_ratio, hidden_dim, n_layers=0, res=False, act=act)
+      #  self.mlp = MLP(hidden_dim, hidden_dim * mlp_ratio, hidden_dim, n_layers=0, res=False, act=act)
         if self.last_layer:
             self.ln_3 = nn.LayerNorm(hidden_dim)
             self.mlp2 = nn.Linear(hidden_dim, out_dim)
 
     def forward(self, fx):
         fx = self.Attn(self.ln_1(fx)) + fx     #[B, num_points, 128] -> [B, num_points, 128] -> [B, num_points, 128]
-        fx = self.mlp(self.ln_2(fx)) + fx
+       # fx = self.mlp(self.ln_2(fx)) + fx      #[B, num_points, 128] -> [B, num_points, 128] -> [B, num_points, 128]
+        fx = self.ln_2(fx) + fx                #[B, num_points, 128] -> [B, num_points, 128]
         if self.last_layer:
             return self.mlp2(self.ln_3(fx))
         else:
@@ -189,6 +220,7 @@ class Model(nn.Module):
                  out_dim=1,
                  slice_num=32,
                  ref=8,
+                 k=40,
                  unified_pos=False
                  ):
         super(Model, self).__init__()
@@ -198,10 +230,11 @@ class Model(nn.Module):
         self.Time_Input = Time_Input
         self.n_hidden = n_hidden
         self.space_dim = space_dim
+        self.k = k
         if self.unified_pos:
-            self.preprocess = MLP(fun_dim + self.ref * self.ref, n_hidden * 2, n_hidden, n_layers=0, res=False, act=act)
+            self.preprocess = MLP(fun_dim + self.ref * self.ref, n_hidden * 2, n_hidden, n_layers=0, res=False, act=act, k=self.k)
         else:
-            self.preprocess = MLP(space_dim, n_hidden * 2, n_hidden, n_layers=0, res=False, act=act)
+            self.preprocess = MLP(space_dim, n_hidden * 2, n_hidden, n_layers=0, res=False, act=act, k=self.k)
         if Time_Input:
             self.time_fc = nn.Sequential(nn.Linear(n_hidden, n_hidden), nn.SiLU(), nn.Linear(n_hidden, n_hidden))
 
