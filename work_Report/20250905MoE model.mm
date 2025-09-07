d18e0aba5c011e3985b1c7bee3d0dae5e725f398
Bojun Zhang <1002129979@qq.com>
2025-09-07 09:09:57 +0800

    20250905MoE model introduction

---
+++ work_Report/20250905MoE model.mm
@@ -0,0 +1,205 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="MoE model" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1757119130420" STYLE="oval" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_icon_for_attributes="true" auto_compact_layout="true" show_tags="UNDER_NODES" associatedTemplateLocation="template:/common.mm" show_note_icons="true" followedTemplateLocation="template:/standard-1.6.mm" followedMapLastTime="1748784985000" fit_to_viewport="false" show_icons="BESIDE_NODES" showTagCategories="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#000000" STYLE="fork" NUMBERED="false" FORMAT="STANDARD_FORMAT" TEXT_ALIGN="DEFAULT" TEXT_WRITING_DIRECTION="LEFT_TO_RIGHT" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt" CHILD_NODES_LAYOUT="AUTO" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#808080" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="Times New Roman" SIZE="10" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+<edge COLOR="#808080"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="13" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Mixture of Experts framework
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="Three pre-trained model" POSITION="bottom_or_right" ID="ID_198016096" CREATED="1756866717926" MODIFIED="1756866734520">
+<edge COLOR="#007c00"/>
+<node TEXT="DoMINO" ID="ID_719559727" CREATED="1756866734676" MODIFIED="1756866777464"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      A decomposable multi-scale neural operator
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="X-MeshGraphNet" ID="ID_479108734" CREATED="1756866741664" MODIFIED="1756866791931"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      A scalable multi-scale graph neural network
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="FigConvNet" ID="ID_433812998" CREATED="1756866747809" MODIFIED="1756866804455"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      A factorized implicit global convolution network
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="Gating Network Architecture" POSITION="bottom_or_right" ID="ID_1429149870" CREATED="1756866819517" MODIFIED="1756868275035" HGAP_QUANTITY="14 pt" VSHIFT_QUANTITY="-33.44882 pt" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt">
+<edge COLOR="#7c007c"/>
+<node TEXT="Input Features" ID="ID_1940045473" CREATED="1756866846665" MODIFIED="1756866914038">
+<node TEXT="Point coordinate" ID="ID_440932643" CREATED="1756867045573" MODIFIED="1756867054096">
+<node TEXT="\latex $(x,y,z)$" ID="ID_209826265" CREATED="1756867063198" MODIFIED="1756867097480"/>
+</node>
+<node TEXT="Pressure" ID="ID_962190300" CREATED="1756866914205" MODIFIED="1756866916888">
+<node TEXT="\latex $P_{DoMINO}$" ID="ID_320754035" CREATED="1756866919083" MODIFIED="1756866931661"/>
+<node TEXT="\latex $P_{FigConvNet}$" ID="ID_1097298006" CREATED="1756866931932" MODIFIED="1756866941343"/>
+<node TEXT="\latex $P_XMGN$" ID="ID_117386928" CREATED="1756866941639" MODIFIED="1756866949162"/>
+</node>
+<node TEXT="Shear Stress" ID="ID_1875358761" CREATED="1756866917228" MODIFIED="1756866983633" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt">
+<node TEXT="\latex $WSS_{DoMINO}$" ID="ID_1787077389" CREATED="1756866960267" MODIFIED="1756866975295"/>
+<node TEXT="\latex $WSS_{FigConvNet}$" ID="ID_559292225" CREATED="1756866979040" MODIFIED="1756866993815"/>
+<node TEXT="\latex $WSS_{XMGN}$" ID="ID_987358951" CREATED="1756866979511" MODIFIED="1756867004957"/>
+</node>
+<node TEXT="Local Geometric Features" ID="ID_563941007" CREATED="1756867006829" MODIFIED="1756867015656">
+<node TEXT="\latex $(n_{x},n_{y},n_{z})$" ID="ID_1481770628" CREATED="1756867016178" MODIFIED="1756867117024"/>
+</node>
+</node>
+<node TEXT="MLP module" ID="ID_391713357" CREATED="1756867148623" MODIFIED="1757122731992" VSHIFT_QUANTITY="-2.26772 pt">
+<node TEXT="3 hidden layer " ID="ID_1905308732" CREATED="1756867159347" MODIFIED="1756867285475">
+<node TEXT="128 neurons" ID="ID_596866485" CREATED="1756867285722" MODIFIED="1756867286837"/>
+<node TEXT="ReLu as activation fuction" ID="ID_728166206" CREATED="1756867287101" MODIFIED="1756867296491"/>
+</node>
+<node TEXT="Output layer" ID="ID_1299760960" CREATED="1756867264119" MODIFIED="1756867307593">
+<node TEXT="3 weights" ID="ID_1162811266" CREATED="1756867307598" MODIFIED="1756867997373"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      by Softmax
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="\latex $(W_{p,1},W_{p,2},W_{p,3})$" ID="ID_355370448" CREATED="1756867331513" MODIFIED="1756867526579"/>
+<node TEXT="\latex $(W_{s,1},W_{s,2},W_{s,3})$" ID="ID_121038060" CREATED="1756867551399" MODIFIED="1756867567939"/>
+</node>
+<node TEXT="Correction term" ID="ID_167760605" CREATED="1756867602607" MODIFIED="1756867619144">
+<node TEXT="\latex $C_{MoE}$" ID="ID_375581849" CREATED="1756867619404" MODIFIED="1756867631356"/>
+</node>
+</node>
+<node TEXT="Math Formula" ID="ID_351328563" CREATED="1756867670210" MODIFIED="1756867686583">
+<node TEXT="\latex $P_{MoE}=W_{p,1}\cdot P_{DoMINO}+W_{p,2}\cdot P_{FigConvNet}+W_{p,3}\cdot P_{XMGN}+C_{p,MoE}$" ID="ID_1165975119" CREATED="1756867680057" MODIFIED="1756867786435"/>
+</node>
+</node>
+<node TEXT="Training with Entropy Regularization" ID="ID_243052826" CREATED="1756867652864" MODIFIED="1756867836267"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Avoid model collapse
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="\latex $H(w)=-\sum_{i=1}^{N}w_{i}\log(w_{i})$" ID="ID_1939475268" CREATED="1756867823965" MODIFIED="1756867955477"/>
+<node TEXT="H(w) is Applied in Loss Function" ID="ID_249115941" CREATED="1756867955931" MODIFIED="1756868041034">
+<node TEXT="\latex $L_{total}=L_{pressure}+L_{shear}-\lambda_{entroly}\cdot (H(w_{pressure})+H(w_{shear}))$" ID="ID_287009173" CREATED="1756868041039" MODIFIED="1756868139304"/>
+<node TEXT="\latex $\lambda_{entropy}=0.01$" ID="ID_192705035" CREATED="1756868140066" MODIFIED="1756868165313"/>
+</node>
+</node>
+</node>
+<node TEXT="Future work" POSITION="bottom_or_right" ID="ID_1656149366" CREATED="1756868175017" MODIFIED="1757120999747" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt">
+<edge COLOR="#7c7c00"/>
+<node TEXT="More Sophisticated Gating Mechanisms" ID="ID_1449416186" CREATED="1756868180194" MODIFIED="1757121001164" VSHIFT_QUANTITY="14.74016 pt">
+<node TEXT="对MLP模块进行扩展" ID="ID_31605587" CREATED="1756868262096" MODIFIED="1756868270366"/>
+</node>
+<node TEXT="Uncertainty Quantification" ID="ID_181209687" CREATED="1756868203680" MODIFIED="1757121004799" VSHIFT_QUANTITY="-1.70079 pt">
+<node TEXT="尝试利用transolver的attention机制进行优化，得到更好的权重w" ID="ID_520492688" CREATED="1756868215280" MODIFIED="1757118960575"/>
+</node>
+</node>
+</node>
+</map>
