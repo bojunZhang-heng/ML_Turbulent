2e346962e8a7940f52950a07cabd2cc6bd8c435f
Bojun Zhang <1002129979@qq.com>
2025-09-13 09:22:16 +0800

    20250911AB-UBT model.* update

---
+++ work_Report/20250911AB-UBT model.mm
@@ -0,0 +1,128 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="AB-UBT model" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1757577987096" STYLE="oval" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_icon_for_attributes="true" auto_compact_layout="true" show_tags="UNDER_NODES" associatedTemplateLocation="template:/common.mm" show_note_icons="true" followedTemplateLocation="template:/standard-1.6.mm" followedMapLastTime="1748784985000" fit_to_viewport="false" show_icons="BESIDE_NODES" showTagCategories="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#000000" STYLE="fork" NUMBERED="false" FORMAT="STANDARD_FORMAT" TEXT_ALIGN="DEFAULT" TEXT_WRITING_DIRECTION="LEFT_TO_RIGHT" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt" CHILD_NODES_LAYOUT="AUTO" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#808080" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="Times New Roman" SIZE="10" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+<edge COLOR="#808080"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="10" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook URI="needed_Picture/AB-UBT_multi-branch.png" SIZE="1.0" NAME="ExternalObject"/>
+<node TEXT="Multi-branch" POSITION="bottom_or_right" ID="ID_1200983140" CREATED="1757574632375" MODIFIED="1757723706203" HGAP_QUANTITY="-78.40945 pt" VSHIFT_QUANTITY="-211.46458 pt" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt">
+<edge COLOR="#007c00"/>
+<richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      corss attention
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="Encoder block" ID="ID_1009362886" CREATED="1757575752594" MODIFIED="1757723885594" HGAP_QUANTITY="18.53544 pt" VSHIFT_QUANTITY="1853.2914 pt" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt">
+<node TEXT="输入信息" ID="ID_1358133663" CREATED="1757575173948" MODIFIED="1757575776304" HGAP_QUANTITY="12.29921 pt" VSHIFT_QUANTITY="73.70078 pt">
+<node TEXT="\latex $X^g$" ID="ID_421327783" CREATED="1757575232298" MODIFIED="1757575239584"/>
+<node TEXT="\latex $X^s$" ID="ID_1693242015" CREATED="1757575240102" MODIFIED="1757575253270"/>
+<node TEXT="\latex $X^v$" ID="ID_165881486" CREATED="1757575253642" MODIFIED="1757575258894"/>
+</node>
+<node TEXT="通过MLP模块" ID="ID_23474568" CREATED="1757575263337" MODIFIED="1757723914427" VSHIFT_QUANTITY="0.56693 pt" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt">
+<node TEXT="\latex $Z^g$" ID="ID_347757753" CREATED="1757575306528" MODIFIED="1757575309264"/>
+<node TEXT="\latex $Z^s$" ID="ID_1329933613" CREATED="1757575311171" MODIFIED="1757723906145" HGAP_QUANTITY="17.9685 pt" VSHIFT_QUANTITY="5.66929 pt"/>
+<node TEXT="\latex $Z^v$" ID="ID_1318011272" CREATED="1757575315158" MODIFIED="1757575317539"/>
+</node>
+<node TEXT="\latex $P^s:(Q(Z^s),K(Z^g),V(Z^g)) \xrightarrow{\text{attention}}Z_{0}^{s}$" ID="ID_129705445" CREATED="1757575419468" MODIFIED="1757575611124"/>
+<node TEXT="\latex $P^v:(Q(Z^v),K(Z^g),V(Z^g)) \xrightarrow{\text{attention}}Z_{0}^{v}$" ID="ID_1486926798" CREATED="1757575611589" MODIFIED="1757723909510" VSHIFT_QUANTITY="-72 pt"/>
+</node>
+<node TEXT="Physics block" ID="ID_91291384" CREATED="1757576388952" MODIFIED="1757723690374" HGAP_QUANTITY="15.70079 pt" VSHIFT_QUANTITY="-1752.37801 pt" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt">
+<node TEXT="\latex $B_{{k}}^s:(Q(Z^s_{{k-1}}),K(Z^s_{{k-1}}),V(Z^s_{{k-1}})) \xrightarrow{\text{attention}}Z_{k}^{&apos;s}&#xa;\\&#xa;\to(Q(Z^{&apos;s}_{{k}}),K(Z^{&apos;v}_{{k}}),V(Z^{&apos;v}_{{k}}))\xrightarrow{\text{{Attention}}}Z^{s}_{{k+1}}$" ID="ID_669964902" CREATED="1757576751782" MODIFIED="1757670282522" VSHIFT_QUANTITY="6.23622 pt">
+<node TEXT="将面场信息作为查询Q的输入" ID="ID_1310290306" CREATED="1757669882405" MODIFIED="1757669902907"/>
+</node>
+<node TEXT="\latex $B_{{k}}^v:(Q(Z^v_{{k-1}}),K(Z^v_{{k-1}}),V(Z^{&apos;s}_{{k-1}})) \xrightarrow{\text{attention}}Z_{k}^{&apos;v}&#xa;\\&#xa;\to(Q(Z^{&apos;v}_{{k}}),K(Z^{&apos;s}_{{k}}),V(Z^{&apos;s}_{{k}}))\xrightarrow{\text{{Attention}}}Z^{v}_{{k+1}}$" ID="ID_922227907" CREATED="1757576755088" MODIFIED="1757670281519" VSHIFT_QUANTITY="16.44095 pt"/>
+<node TEXT="switch _attention_1" ID="ID_405764315" CREATED="1757669400157" MODIFIED="1757669831417" VSHIFT_QUANTITY="43.65354 pt">
+<node TEXT="单纯调整 Q和K-V的输入" ID="ID_1238768050" CREATED="1757669413244" MODIFIED="1757669447523"/>
+<node TEXT="将几何信息作为查询Q的输入，去得到面场所蕴含的数据信息" ID="ID_526170398" CREATED="1757669647898" MODIFIED="1757669689547"/>
+</node>
+<node TEXT="switch attention_2" ID="ID_97544944" CREATED="1757585070877" MODIFIED="1757669833542" VSHIFT_QUANTITY="-8.50394 pt">
+<node TEXT="提高计算效率" ID="ID_175109093" CREATED="1757669143067" MODIFIED="1757669157760"/>
+<node TEXT="底层是MoE模型" ID="ID_1075696836" CREATED="1757669104648" MODIFIED="1757669131572"/>
+<node TEXT="为每个输入动态选择一个专家模型" ID="ID_1947483875" CREATED="1757669131939" MODIFIED="1757669203358"/>
+</node>
+</node>
+</node>
+<node TEXT="" POSITION="top_or_left" ID="ID_718289451" CREATED="1757578203979" MODIFIED="1757670000303" HGAP_QUANTITY="32.14173 pt" VSHIFT_QUANTITY="-58.39371 pt">
+<edge COLOR="#007c7c"/>
+<hook URI="needed_Picture/AB-UBT_multi-branch_1.png" SIZE="0.5263158" NAME="ExternalObject"/>
+</node>
+</node>
+</map>
