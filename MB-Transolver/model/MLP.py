6ee0efd6c4cf21b8cc585e6ac8263726fdf80174
mae-zhangbj <1002129979@qq.com>
2025-09-30 13:12:16 +0800

    model/MLP.py Just one MLP for all encode

--- MB-Transolver/model/MLP.py
+++ MB-Transolver/model/MLP.py
@@ -2,14 +2,26 @@ import torch
 from torch import nn
 
 class MLP(nn.Module):
-    def __init__(self, args):
+    def __init__(
+            self,
+            res,
+            n_input,
+            n_hidden,
+            n_output,
+            n_layers
+        ):
         super(MLP, self).__init__()
 
-        self.n_layers = args.n_layers
-        self.res = args.res
-        self.linear_pre = nn.Sequential(nn.Linear(args.n_dim, args.n_hidden), nn.GELU())
-        self.linear_post = nn.Linear(args.n_hidden, args.n_hidden)
-        self.linears = nn.ModuleList([nn.Sequential(nn.Linear(args.n_hidden, args.n_hidden), nn.GELU()) for _ in range(args.n_layers)])
+        self
+
+        self.n_layers = n_layers
+        self.res = res
+        self.n_input = n_input
+        self.n_hidden = n_hidden
+        self.n_output = n_output
+        self.linear_pre = nn.Sequential(nn.Linear(self.n_input, self.n_hidden), nn.GELU())
+        self.linear_post = nn.Linear(self.n_hidden, self.n_output)
+        self.linears = nn.ModuleList([nn.Sequential(nn.Linear(self.n_hidden, self.n_hidden), nn.GELU()) for _ in range(self.n_layers)])
 
     def forward(self, x):
         x = self.linear_pre(x)
