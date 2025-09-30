aeb14ff2bed032829a5b02589266ee73110b8667
mae-zhangbj <1002129979@qq.com>
2025-09-30 12:15:11 +0800

    model/ Geometry attention Update

---
+++ MB-Transolver/model/Driver_MBTransolver.py
@@ -0,0 +1,48 @@
+from functools import partial
+
+import einops
+import torch
+from torch import nn
+
+from modules.attention import (
+    AnchorAttention,
+    SharedweightsCrossattnAttention,
+    SharedweightsSplitattnAttention,
+)
+from model.Transolver_block import Transolver_block
+from model.MLP import MLP
+from modules.continuous_sincos_embed import ContinuousSincosEmbed
+from modules.rope_frequency import RopeFrequency
+
+class Model(nn.Module):
+    def __init__(self, args):
+        super().__init__()
+    #    self.rope = RopeFrequency(dim=dim // num_heads, ndim=input_dim)
+        # geometry
+        self.preprocess = MLP(args)
+        self.geometry_blocks = nn.ModuleList(
+            [
+                Transolver_block(args, last_layer=(_ == args.geometry_depth - 1))
+                for _ in range(args.geometry_depth)
+            ]
+        )
+
+    def forward(
+            self,
+            # Geometry
+            geometry_position: torch.Tensor,
+            # Surface
+            wss_position: torch.Tensor,
+            wss_value: torch.Tensor,
+            pressure_position: torch.Tensor,
+            pressure_value: torch.Tensor
+    )-> dict[str, torch.Tensor]:
+        geometry_position = self.preprocess(geometry_position)
+
+        for geometry_block in self.geometry_blocks:
+            geometry_position = geometry_block(geometry_position)
+
+
+        return geometry_position
+
+
