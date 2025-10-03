7970d394a16b2e64b3cde642733fb90f7d909182
mae-zhangbj <1002129979@qq.com>
2025-10-03 17:27:54 +0800

    src/train_one_epoch.py shareweights attention Update

--- MB-Transolver/src/train_one_epoch.py
+++ MB-Transolver/src/train_one_epoch.py
@@ -11,15 +11,24 @@ def train_one_epoch(model, train_dataloader, optimizer, criterion, local_rank):
 
     for combined_data in tqdm(train_dataloader, desc="[Training]"):
         data = dict(
-            geometry_position = combined_data["geometry"],
-            wss_position = combined_data["wss_point"],
-            wss_value = combined_data["wss_value"],
-            pressure_position = combined_data["pressure_point"],
-            pressure_value = combined_data["pressure_value"]
+            geometry_position = combined_data["geometry_position"],
+            surf_position = combined_data["surf_position"],
+            surf_pressure = combined_data["surf_pressure"],
+            surf_wss = combined_data["surf_wss"],
+            volume_position = combined_data["volume_position"],
+            volume_pressure = combined_data["volume_pressure"],
+            volume_wss = combined_data["volume_wss"],
+            volume_vel = combined_data["volume_vel"]
         )
 
         data["geometry_position"] = data["geometry_position"].squeeze(1).to(local_rank)
         data["geometry_position"] = data["geometry_position"].permute(0, 2 ,1).contiguous()
+
+        data["surf_position"] = data["surf_position"].squeeze(1).to(local_rank)
+        data["surf_position"] = data["surf_position"].permute(0, 2 ,1).contiguous()
+
+        data["volume_position"] = data["volume_position"].squeeze(1).to(local_rank)
+        data["volume_position"] = data["volume_position"].permute(0, 2 ,1).contiguous()
 
         optimizer.zero_grad()
         outputs = model(**data)
