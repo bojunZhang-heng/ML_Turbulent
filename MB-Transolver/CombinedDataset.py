143a60c9ca0d6dc1dcf0ccdac476238593c1f1b9
mae-zhangbj <1002129979@qq.com>
2025-10-03 17:24:13 +0800

    CombinedDataset.py Update

--- MB-Transolver/CombinedDataset.py
+++ MB-Transolver/CombinedDataset.py
@@ -2,10 +2,11 @@ import torch
 from torch.utils.data import Dataset
 
 class CombinedDataset(Dataset):
-    def __init__(self, pressure_dataset, wss_dataset, cad_dataset):
+    def __init__(self, pressure_dataset, wss_dataset, cad_dataset, volume_dataset):
         self.pressure_dataset = pressure_dataset
         self.wss_dataset = wss_dataset
         self.cad_dataset = cad_dataset
+        self.volume_dataset = volume_dataset
 
     def __len__(self):
         return len(self.pressure_dataset)  # Assuming all datasets have the same length
@@ -15,14 +16,18 @@ class CombinedDataset(Dataset):
         pressure_data = self.pressure_dataset[idx]
         wss_data = self.wss_dataset[idx]
         cad_data = self.cad_dataset[idx]
+        volume_data = self.volume_dataset[idx]
 
         # Combine the data into a single dictionary
         combined_data = {
-            'pressure_point': pressure_data[0],
-            'pressure_value': pressure_data[1],
-            'wss_point': wss_data[0],
-            'wss_value': wss_data[1],
-            'geometry': cad_data,
+            'surf_position': pressure_data[0],
+            'surf_pressure': pressure_data[1],
+            'surf_wss': wss_data[1],
+            'geometry_position': cad_data,
+            'volume_position': volume_data[0],
+            'volume_pressure': volume_data[1],
+            'volume_wss': volume_data[2],
+            'volume_vel': volume_data[3],
         }
         return combined_data
 
