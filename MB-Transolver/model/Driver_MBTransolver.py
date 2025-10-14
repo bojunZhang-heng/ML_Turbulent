68e8fe366f020bba553ae497c8f8dc052b3a677d
mae-zhangbj <1002129979@qq.com>
2025-10-14 13:34:45 +0800

    model/Driver_MBTransolver.py add ROPE part Update

--- MB-Transolver/model/Driver_MBTransolver.py
+++ MB-Transolver/model/Driver_MBTransolver.py
@@ -1,18 +1,17 @@
-from functools import partial
 
 import einops
 import torch
 import logging
-from torch import nn
 
-from modules.attention import (
-    AnchorAttention,
-    SharedweightsCrossattnAttention,
-    SharedweightsSplitattnAttention,
-)
-from model.transolver_block import TransolverBlock
-from model.sharedweights_split_attention import SharedweightsSplitAttention
+from torch import nn
+from functools import partial
 from model.MLP import MLP
+from model.transolver_block import TransolverBlock
+from model.perceiver_block import PerceiverBlock
+from model.perceiver_attention import PerceiverAttention
+from model.sharedweights_split_attention import SharedweightsSplitAttention
+from model.sharedweights_cross_attention import SharedweightsCrossAttention
+
 from modules.continuous_sincos_embed import ContinuousSincosEmbed
 from modules.rope_frequency import RopeFrequency
 
@@ -20,18 +19,19 @@ class Model(nn.Module):
     def __init__(self, args):
         super().__init__()
 
+        self.rope = RopeFrequency(dim=args.n_hidden // agrs.n_heads , ndim=args.n_dim)
         # geometry
-        self.preprocess = MLP(n_input=args.n_dim, n_hidden=args.n_hidden, n_output=args.n_hidden, n_layers=args.n_layers, res=args.res)
+        self.preprocess = MLP(n_input=args.n_dim, n_hidden=args.n_hidden, n_output=args.n_hidden, n_layers=args.n_layers // 2, res=args.res)
         self.geometry_blocks = nn.ModuleList(
             [
-                TransolverBlock(args, last_layer=(_ == args.geometry_depth - 1))
+                TransolverBlock(args, last_layer=(_ != args.geometry_depth - 1))
                 for _ in range(args.geometry_depth)
             ]
         )
 
-        # pos_embed with separate MLP for surface/volume
-#        self.pos_embed = ContinuousSincosEmbed(ndim=args.n_dim, dim=args.n_hidden)
-        self.pos_embed = MLP(n_input=args.n_dim, n_hidden=args.n_hidden, n_output=args.n_hidden, n_layers=args.n_layers, res=args.res)
+       # pos_embed with separate MLP for surface/volume
+        #self.pos_embed = ContinuousSincosEmbed(ndim=args.n_dim, dim=args.n_hidden)
+        self.pos_embed = MLP(n_input=args.n_dim, n_hidden=args.n_hidden, n_output=args.n_hidden, n_layers=args.n_layers // 2, res=args.res)
         self.surface_bias = nn.Sequential(
             nn.Linear(args.n_hidden, args.n_hidden),
             nn.GELU(),
@@ -44,24 +44,41 @@ class Model(nn.Module):
         )
 
         # weight-shared blocks
+        self.perceiver = PerceiverBlock(args=args, attn_ctor=PerceiverAttention)
         self.blocks = nn.ModuleList()
         for block in args.blocks:
             if block == "s":
                 # weight-shared self-attention within surface/volume tokens
                 block_ctor = partial(TransolverBlock, attn_ctor=SharedweightsSplitAttention)
                 for _ in range(args.geometry_depth):
-                    is_last = (_ == args.geometry_depth - 1)
+                    is_last = (_ != args.geometry_depth - 1)
                     self.blocks.append(block_ctor(args, last_layer=is_last))
             elif block == "c":
                 # weight-shared cross-attention between surface/volume tokens
-                block_ctor = partial(TransformerBlock, attn_ctor=SharedweightsCrossattnAttention)
-                self.blocks.append(block_ctor(args))
-            elif block == "p":
-                # weight-shared cross-attention from surface/volume tokens to geometry tokens
-                block_ctor = PerceiverBlock
-                self.blocks.append(block_ctor(args))
+                block_ctor = partial(TransolverBlock, attn_ctor=SharedweightsCrossAttention)
+                for _ in range(args.geometry_depth):
+                    is_last = (_ != args.geometry_depth - 1)
+                    self.blocks.append(block_ctor(args, last_layer=is_last))
             else:
                 raise NotImplementedError
+
+        # surface-specific blocks
+        self.surface_blocks = nn.ModuleList(
+            [
+                TransolverBlock(args, last_layer=False)
+                for _ in range(args.surf_depth)
+            ]
+        )
+        self.surface_decoder = nn.Linear(args.n_hidden, args.output_dim_surface)
+
+        # volume-specific blocks
+        self.volume_blocks = nn.ModuleList(
+            [
+                TransolverBlock(args, last_layer=False)
+                for _ in range(args.volume_depth)
+            ]
+        )
+        self.volume_decoder = nn.Linear(args.n_hidden, args.output_dim_volume)
 
     def forward(
             self,
@@ -69,6 +86,7 @@ class Model(nn.Module):
             geometry_position: torch.Tensor,
             # Surface
             surf_position: torch.Tensor,
+            surf_position_2: torch.Tensor,
             surf_wss: torch.Tensor,
             surf_pressure: torch.Tensor,
             # Volume
@@ -78,7 +96,15 @@ class Model(nn.Module):
             volume_vel: torch.Tensor
     )-> dict[str, torch.Tensor]:
 
+        outputs = {}
+        geometry_attn_kwargs = {}
+        surface_decoder_attn_kwargs = {}
+        volume_decoder_attn_kwargs = {}
+        geometry_perceiver_attn_kwargs = {}
+        shared_attn_kwargs = {}
+
         # Create split size
+        volume_position = surf_position_2
         split_size = [surf_position.size(1), volume_position.size(1)]
 
         # Encode geometry
@@ -94,14 +120,42 @@ class Model(nn.Module):
 
         surface_pos_embed = self.surface_bias(self.pos_embed(surf_position))
         volume_pos_embed = self.volume_bias(self.pos_embed(volume_position))
-        x = torch.concat([surface_pos_embed, volume_pos_embed], dim=1)
-        logging.info(f"x.shape: {x.shape}")
+
+#        logging.info(f"surface_pos_embed: {surface_pos_embed.shape}")
+#        logging.info(f"volume_pos_embed: {volume_pos_embed.shape}")
+#        logging.info(f"geometry_encoding: {geometry_encoding.shape}")
+
+        # perceiver_block
+        x_surf, x_volume = self.perceiver(surface_pos_embed, volume_pos_embed, geometry_encoding)
+        x = torch.concat([x_surf, x_volume], dim=1)
+#        logging.info(f"x.shape: {x.shape}")
+
         for block in self.blocks:
             x = block(x, attn_kwargs=dict(split_size=split_size))
 
         x_surf, x_volume = x.split(split_size, dim=1)
 
+        # surface blocks
+        for block in self.surface_blocks:
+            x_surf = block(x_surf)
+        x_surf = self.surface_decoder(x_surf)            #(6, 10000, 3)
 
-        return geometry_position
+        outputs = {}
+        outputs["surf_pressure"] = x_surf                # (B, N, pressure_dim)
+#        outputs["surf_pressure"] = x_surf[..., 0]
+#        outputs["surf_wss"] = x_surf[..., 1:]
+
+        # volume blocks
+        for block in self.volume_blocks:
+            x_volume = block(x_volume)
+        x_volume = self.volume_decoder(x_volume)             #(6, 10000, 3)
+
+        outputs["surf_wss"] = x_volume                       #(6, 10000, 3)
+#        outputs["volume_pressure"] = x_volume[..., 0]
+#        outputs["volume_wss"] = x_volume[..., 1:3]
+#        outputs["volume_vel"] = x_volume[..., 4:]
+
+
+        return outputs
 
 
