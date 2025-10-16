a0b715cccec0cad9cad8e8cd887444e38092499b
mae-zhangbj <1002129979@qq.com>
2025-10-16 16:35:13 +0800

    model/transolver_block.py Update

--- MB-Transolver/model/transolver_block.py
+++ MB-Transolver/model/transolver_block.py
@@ -7,16 +7,13 @@ from model.MLP import MLP
 class TransolverBlock(nn.Module):
     """Transformer encoder block."""
 
-    def __init__(self, args, last_layer, attn_ctor: type[nn.Module]=PhysicsAttention):
+    def __init__(self, args, attn_ctor: type[nn.Module]=PhysicsAttention):
         super().__init__()
         self.last_layer = last_layer
         self.ln_1 = nn.LayerNorm(args.n_hidden)                      # C = 128
         self.Attn = attn_ctor(args)                                  # C = 4*64 = 256
         self.ln_2 = nn.LayerNorm(args.n_hidden)
         self.mlp = MLP(n_input=args.n_hidden, n_hidden=args.n_hidden, n_output=args.n_hidden, n_layers=3, res=args.res)
-        if self.last_layer:
-            self.ln_3 = nn.LayerNorm(args.n_hidden)
-            self.mlp2 = nn.Linear(args.n_hidden, args.n_dim)
 
     def forward(
             self,
@@ -31,10 +28,8 @@ class TransolverBlock(nn.Module):
         Returns: the size is not determined
             (batch_size, num_tokens, dim)
         """
-        x = self.Attn(self.ln_1(x), **(attn_kwargs or {})) + x
-        x = self.mlp(self.ln_2(x)) + x
-        if self.last_layer:
-            return self.mlp2(self.ln_3(x))
-        else:
-            return x
+        x = x + self.Attn(self.ln_1(x), **(attn_kwargs or {}))
+        x = x + self.mlp(self.ln_2(x))
+
+        return x
 
