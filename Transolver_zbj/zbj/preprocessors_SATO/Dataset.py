fabb814c11b9a9d13908b4173c98f644cc7c1a48
mae-zhangbj <1002129979@qq.com>
2025-12-10 09:04:32 +0800

    preprocessors_SATO/Dataset.py Update

--- Transolver_zbj/zbj/preprocessors_SATO/Dataset.py
+++ Transolver_zbj/zbj/preprocessors_SATO/Dataset.py
@@ -95,8 +95,8 @@ class VTKDataset():
         train_data_lst, test_data_lst, val_data_lst = [], [], []
         for file_path in SurfacePressure_file_paths:
             index = file_path.split("_")[-1].split(".")[0]
-            Surface_points = np.load(os.path.join(directory, 'SurfacePressure', 'points', f'points_{index}.npy'))
-            Surface_pressure = np.load(os.path.join(directory, 'SurfacePressure', 'pressure', f'pressure_{index}.npy'))
+            Surface_points = np.load(os.path.join(directory, 'SurfacePressure', 'points_v2', f'points_{index}.npy'))
+            Surface_pressure = np.load(os.path.join(directory, 'SurfacePressure', 'pressure_v2', f'pressure_{index}.npy'))
 
             Surface_points = torch.Tensor(Surface_points).float()
 #            Surface_points = (Surface_points - POS_MIN) / (POS_MAX - POS_MIN) * 1000
