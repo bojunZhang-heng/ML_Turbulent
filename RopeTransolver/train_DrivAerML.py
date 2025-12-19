b2d547bb171b9a1d0e35783daeb0bb3a47f56fd0
mae-zhangbj <1002129979@qq.com>
2025-12-19 12:47:50 +0800

    RopeTransolver/train_DrivAerML.py Update

--- RopeTransolver/train_DrivAerML.py
+++ RopeTransolver/train_DrivAerML.py
@@ -4,7 +4,6 @@ import os
 import yaml
 import argparse
 import torch
-import torch.distributed as dist
 import torch.optim as optim
 import time
 import logging
@@ -16,7 +15,7 @@ from types import SimpleNamespace
 # from torch.utils.data.distributed import DistributedSampler
 from utils_v1 import setup_logger, setup_seed
 from colorama import Fore, Style
-from model_transolver import Model
+from modules_RT.model.model_transolver import Model
 
 # from model_tmp import AnchoredBranchedUPT
 from preprocessors_DrivAerML import (
