be292ec0ffa49545b68fca7ece91a1c336c617b3
mae-zhangbj <1002129979@qq.com>
2025-12-21 09:29:42 +0800

    Transolver_zbj/zbj/submit.sh Update

--- Transolver_zbj/zbj/submit.sh
+++ Transolver_zbj/zbj/submit.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# cuda:3 for DrivAerNet
+# cuda:4 for DrivAerML velocity run_*
+# cuda:5 for DrivAerNet
 
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
-export CUDA_VISIBLE_DEVICES=5
+export CUDA_VISIBLE_DEVICES=4
 
 # 确保 logs 目录存在
 LOG_DIR=./logs
@@ -12,11 +15,11 @@ TIMESTAMP=$(date "+%Y_%m%d_%H%M")
 
 # 启动训练（后台）
 # DrivAerML
-#nohup python train_DrivAerML.py config_DrivAerML_spressure.yml > ${LOG_DIR}/DrivAerML_spressure_${TIMESTAMP}.log 2>&1 &
-nohup python train_DrivAerML.py config_DrivAerML_velocity.yml > ${LOG_DIR}/DrivAerML_velocity_${TIMESTAMP}.log 2>&1 &
+nohup python train_DrivAerML.py config_DrivAerML_spressure.yml > ${LOG_DIR}/DrivAerML_spressure_${TIMESTAMP}.log 2>&1 &
+#nohup python train_DrivAerML.py config_DrivAerML_velocity.yml > ${LOG_DIR}/DrivAerML_velocity_${TIMESTAMP}.log 2>&1 &
 
 # DrivAerNet
-#nohup python train_drivaernet.py config_DrivAerNet_spressure.yml > ${LOG_DIR}/DrivAerNet_spressure_${TIMESTAMP}.log 2>&1 &
+#nohup python train_DrivAerNet.py config_DrivAerNet_spressure.yml > ${LOG_DIR}/DrivAerNet_spressure_${TIMESTAMP}.log 2>&1 &
 #nohup python train_drivaernet.py config_DrivAerNet_swss.yml > ${LOG_DIR}/DrivAerNet_swss_${TIMESTAMP}.log 2>&1 &
 
 
