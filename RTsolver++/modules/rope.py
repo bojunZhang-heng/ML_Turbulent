1df32b605b4db1471065fc12edc32553ea252e33
mae-zhangbj <1002129979@qq.com>
2025-12-28 14:30:40 +0800

    RTsolver++/modules/rope.py Update

--- RTsolver++/modules/rope.py
+++ RTsolver++/modules/rope.py
@@ -1,5 +1,6 @@
 import einops
 import torch
+import logging
 
 
 def rope(x: torch.Tensor, freqs: torch.Tensor) -> torch.Tensor:
@@ -8,7 +9,7 @@ def rope(x: torch.Tensor, freqs: torch.Tensor) -> torch.Tensor:
     assert torch.is_tensor(freqs) and torch.is_complex(freqs)
     assert x.ndim == 4, "x.shape should be (batch_size, num_heads, seqlen, head_dim)"
     assert freqs.ndim == 3, "freqs.shape should be (batch_size, seqlen, head_dim // 2)"
-    x_ = torch.view_as_complex(x.float().reshape(*x.shape[:-1], -1, 2))
+    x_ = torch.view_as_complex(x.float().reshape(*x.shape[:-1], -1, 2)) # [1, 8, 10000, 16]
     # add dim for num_heads
     freqs = einops.rearrange(freqs, "batch_size seqlen head_dim -> batch_size 1 seqlen head_dim")
     x_out = torch.view_as_real(x_ * freqs).flatten(start_dim=3)
