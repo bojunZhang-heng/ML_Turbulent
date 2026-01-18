d109dd08eaed85875cd7e077d6a99c188a1dc471
Bojun Zhang <1002129979@qq.com>
2026-01-18 19:32:04 +0800

    modules_RT/physical_attention.py Update

--- RopeTransolver/modules_RT/physical_attention.py
+++ RopeTransolver/modules_RT/physical_attention.py
@@ -21,20 +21,16 @@ class Physics_Attention_Irregular_Mesh(nn.Module):
         self.dropout = nn.Dropout(dropout)
         self.temperature = nn.Parameter(torch.ones([1, head_num, 1, 1]) * 0.5)
 
-        self.in_project_x = nn.Linear(dim, dim)
-        self.in_project_fx = nn.Linear(dim, dim)
-        self.in_project_slice = nn.Linear(head_dim, slice_num)
-
-        self.in_project_freqs = nn.Linear(dim, dim)
-        self.in_project_ffreqs = nn.Linear(dim, dim)
-        self.in_project_freqs_slice = nn.Linear(head_dim, slice_num)
+        self.in_project_x = nn.Linear(self.head_dim, self.head_dim)
+        self.in_project_fx = nn.Linear(self.head_dim, self.head_dim)
+        self.in_project_freqs = nn.Linear(self.head_dim, self.head_dim)
+       
+        self.in_project_slice = nn.Linear(self.head_dim, slice_num)
 
         for l in [self.in_project_slice]:
             torch.nn.init.orthogonal_(l.weight)  # use a principled initialization
 
-        for l in [self.in_project_freqs_slice]:
-            torch.nn.init.orthogonal_(l.weight)  # use a principled initialization
-        self.to_q = nn.Linear(head_dim, head_dim, bias=False)
+        self.to_q = nn.Linear(self.head_dim, self.head_dim, bias=False)
         self.to_k = nn.Linear(head_dim, head_dim, bias=False)
         self.to_v = nn.Linear(head_dim, head_dim, bias=False)
         self.rope_x = nn.Linear(dim, dim*3)
@@ -49,31 +45,30 @@ class Physics_Attention_Irregular_Mesh(nn.Module):
         # complex -> real
         freqs = torch.view_as_real(freqs)
         freqs = freqs.reshape(B, N, 2*freqs.shape[2])
+
+        # share weights among x and freqs
+        freqs = freqs.reshape(B, N, self.head_num, self.head_dim) \
+            .permute(0, 2, 1, 3).contiguous()
+        x = x.reshape(B, N, self.head_num, self.head_dim) \
+            .permute(0, 2, 1, 3).contiguous()
         #logging.info(f"freqs: {freqs.shape}")
 
         ### (1) Slice
-        fx_mid = self.in_project_fx(x).reshape(B, N, self.head_num, self.head_dim) \
-            .permute(0, 2, 1, 3).contiguous()  # B H N C
-        x_mid = self.in_project_x(x).reshape(B, N, self.head_num, self.head_dim) \
-            .permute(0, 2, 1, 3).contiguous()  # B H N C
+        fx_mid = self.in_project_fx(x).contiguous()  # B H N C
+        freqs_mid = self.in_project_freqs(freqs).contiguous()  # B H N C
+        x_mid = self.in_project_x(x).contiguous()  # B H N C
         slice_weights = self.softmax(self.in_project_slice(x_mid) / self.temperature)  # B H N G
         slice_norm = slice_weights.sum(2)  # B H G
-        slice_token = torch.einsum("bhnc,bhng->bhgc", fx_mid, slice_weights)
-        slice_token = slice_token / ((slice_norm + 1e-5)[:, :, :, None].repeat(1, 1, 1, self.head_dim))
-
-        freqs_mid = self.in_project_freqs(freqs).reshape(B, N, self.head_num, self.head_dim) \
-            .permute(0, 2, 1, 3).contiguous()  # B H N C
-        ffreqs_mid = self.in_project_ffreqs(freqs).reshape(B, N, self.head_num, self.head_dim) \
-            .permute(0, 2, 1, 3).contiguous()  # B H N C
-        slice_weights_freqs = self.softmax(self.in_project_slice(freqs_mid) / self.temperature)  # B H N G
-        slice_norm_freqs = slice_weights_freqs.sum(2)  # B H G
-        slice_token_freqs = torch.einsum("bhnc,bhng->bhgc", ffreqs_mid, slice_weights_freqs)
-        slice_token_freqs = slice_token_freqs / ((slice_norm_freqs + 1e-5)[:, :, :, None].repeat(1, 1, 1, self.head_dim))
+        
+        slice_token_x = torch.einsum("bhnc,bhng->bhgc", fx_mid, slice_weights)
+        slice_token_x = slice_token_x / ((slice_norm + 1e-5)[:, :, :, None].repeat(1, 1, 1, self.head_dim))
+        slice_token_freqs = torch.einsum("bhnc,bhng->bhgc", freqs_mid, slice_weights)
+        slice_token_freqs = slice_token_freqs / ((slice_norm + 1e-5)[:, :, :, None].repeat(1, 1, 1, self.head_dim))
 
         ### (2) Attention among slice tokens
-        q_slice_token = self.to_q(slice_token)
-        k_slice_token = self.to_k(slice_token)
-        v_slice_token = self.to_v(slice_token)
+        q_slice_token = self.to_q(slice_token_x)
+        k_slice_token = self.to_k(slice_token_x)
+        v_slice_token = self.to_v(slice_token_x)
 
         q_slice_token = rope(q_slice_token, freqs=slice_token_freqs)
         k_slice_token = rope(k_slice_token, freqs=slice_token_freqs)
@@ -81,7 +76,7 @@ class Physics_Attention_Irregular_Mesh(nn.Module):
         dots = torch.matmul(q_slice_token, k_slice_token.transpose(-1, -2)) * self.scale
         attn = self.softmax(dots)
         attn = self.dropout(attn)
-        out_slice_token = torch.matmul(attn, v_slice_token)  # B H G D
+        out_slice_token = torch.matmul(attn, v_slice_token)  # B H G C
 
         ### (3) Deslice
         out_x = torch.einsum("bhgc,bhng->bhnc", out_slice_token, slice_weights)
