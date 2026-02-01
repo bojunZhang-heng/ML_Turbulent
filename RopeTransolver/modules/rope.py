8b356de8080b3b6829a7e26e7e7b7880487a7ef8
Bojun Zhang <1002129979@qq.com>
2026-02-01 18:08:26 +0800

    modules/rope.py Update

--- RopeTransolver/modules/rope.py
+++ RopeTransolver/modules/rope.py
@@ -10,6 +10,7 @@ def rope(x: torch.Tensor, freqs: torch.Tensor) -> torch.Tensor:
     x_ = torch.view_as_complex(x.float().reshape(*x.shape[:-1], -1, 2))
     freqs_ = torch.view_as_complex(freqs.float().reshape(*freqs.shape[:-1], -1, 2).contiguous())
     x_out = torch.view_as_real(x_ * freqs_).flatten(start_dim=3)
+
     return x_out.type_as(x)
 
 
