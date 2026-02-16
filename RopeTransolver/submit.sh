d8411494688203dabcfa969fd8d774b51ae03ba6
Bojun Zhang <1002129979@qq.com>
2026-02-16 10:18:50 +0800

    submit.sh Update

--- RopeTransolver/submit.sh
+++ RopeTransolver/submit.sh
@@ -34,9 +34,14 @@ TIMESTAMP=$(date "+%Y_%m%d_%H%M")
 # DrivAerNet++
 #nohup python train_DrivAerNet++.py config_DrivAerNet++_WW_spressure.yml > ${LOG_DIR}/DrivAerNet++_WW_spressure_${TIMESTAMP}.log 2>&1 &
 #nohup python train_DrivAerNet++.py config_DrivAerNet++_WWC_spressure.yml > ${LOG_DIR}/DrivAerNet++_WWC_spressure_${TIMESTAMP}.log 2>&1 &
-nohup python train_DrivAerNet++.py config_DrivAerNet++_WW_swss.yml > ${LOG_DIR}/DrivAerNet++_WW_swss_${TIMESTAMP}.log 2>&1 &
+#nohup python train_DrivAerNet++.py config_DrivAerNet++_WW_swss.yml > ${LOG_DIR}/DrivAerNet++_WW_swss_${TIMESTAMP}.log 2>&1 &
 #nohup python train_DrivAerNet++.py config_DrivAerNet++_WWC_swss.yml > ${LOG_DIR}/DrivAerNet++_WWC_swss_${TIMESTAMP}.log 2>&1 &
 
+# ShapeNet
+nohup python train_AhmedBody.py config_AhmedBody.yml > ${LOG_DIR}/AhmedBody_${TIMESTAMP}.log 2>&1 &
+
+# AhmedBody
+#nohup python train_ShapeNet.py config_ShapeNet.yml > ${LOG_DIR}/ShapeNet_${TIMESTAMP}.log 2>&1 &
 
 # 获取训练脚本 PID
 PID=$!
