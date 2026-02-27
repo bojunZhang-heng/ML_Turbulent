3faa921a67abf7a3d3514f440cacd2231f9e8f42
Bojun Zhang <1002129979@qq.com>
2026-02-28 07:07:53 +0800

    RopeTransolver/utils_RPTO.py

--- RopeTransolver/utils_RPTO.py
+++ RopeTransolver/utils_RPTO.py
@@ -1,130 +1,79 @@
+import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-import torch
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401
 
-def vistualization(
-    pos,
-    color,
-    title=None,
-    alpha=0.5,
-    num_points=10000,
-    figsize=(18, 6),
-    delta_clamp=None,
-    save_path=None
+def plot_car_results(
+    x,
+    y,
+    y_hat,
+    save_path,
+    figsize=(8, 6),
+    point_size=2,
+    colorbar_pad=-0.2,       # colorbar与图像下方的距离
+    colorbar_fontsize=12,
 ):
-    perm = torch.randperm(len(pos[0]), generator=torch.Generator().manual_seed(0))[:num_points]
-    plt.close()
-    plt.clf()
-    view_rotation = [20, 125, 0]
-    fig = plt.figure(figsize=figsize)
-    axs = []
-    vmin = None
-    vmax = None
-    delta = color[1] - color[0]
-    if delta_clamp is not None:
-        delta = delta.clamp(*delta_clamp)
-    scatters = []
+    """
+    论文风格点云可视化：
+    - 完全隐藏3D坐标轴
+    - colorbar 横向紧贴图像下方，长度与图像宽度对齐
+    - 点云自动铺满图像区域
+    """
 
-    # ---- 用 GridSpec 定义子图布局 ----
-    gs = gridspec.GridSpec(2, 3, height_ratios=[1, 0.05], hspace=0.05)
-    axs = []
-    axs.append(fig.add_subplot(gs[0,0], projection='3d'))
-    axs.append(fig.add_subplot(gs[0,1], projection='3d'))
-    axs.append(fig.add_subplot(gs[0,2], projection='3d'))
+    x = np.asarray(x)
+    y = np.asarray(y)
+    y_hat = np.asarray(y_hat)
+    N = x.shape[1]
 
-    sub_tags = ["(a)", "(b)", "(c)"]
-    caption_text = ""
+    coords = x.reshape(N, 3)
+    y_true = y.reshape(N)
+    y_pred = y_hat.reshape(N)
+    abs_err = np.abs(y_true - y_pred)
 
-    for i in range(len(pos) + 1):
-        is_delta = i == 2
+    # 自动判断车长/车宽/车高
+    ranges = coords.max(axis=0) - coords.min(axis=0)
+    sorted_axes = np.argsort(ranges)[::-1]
+    long_axis, mid_axis, short_axis = sorted_axes
 
-        if is_delta:
-            x, y, z = pos[0][perm].unbind(-1)
-        else:
-            x, y, z = pos[i][perm].unbind(-1)
+    X = coords[:, long_axis]
+    Y = coords[:, mid_axis]
+    Z = coords[:, short_axis]
 
-        if vmin is None:
-            vmin = color[i].min()
-            vmax = color[i].max()
+    x_range, y_range, z_range = X.max()-X.min(), Y.max()-Y.min(), Z.max()-Z.min()
+    elev, azim = 25, -135
 
-        scatter = axs[i].scatter(
-            x, y, z, s=3,
-            c=delta[perm] if is_delta else color[i][perm],
-            cmap="coolwarm",
-            vmin=None if is_delta else vmin,
-            vmax=None if is_delta else vmax,
-            alpha=alpha,
-        )
-        scatters.append(scatter)
-#        axs[i].set_xlabel("X Axis", labelpad=10) # 增加 X 轴标签填充
-#        axs[i].set_ylabel("Y Axis", labelpad=10) # 增加 Y 轴标签填充
-#        axs[i].set_zlabel("Z Axis", labelpad=10) # 增加 Z 轴标签填充
-        axs[i].view_init(*view_rotation)
+    vmin = min(y_true.min(), y_pred.min())
+    vmax = max(y_true.max(), y_pred.max())
 
-        if title is not None:
-            if is_delta:
-                caption_text = f"{sub_tags[i]} delta"
-            else:
-                caption_text = f"{sub_tags[i]} {title[i]}"
+    def draw(values, filename, vmin=None, vmax=None):
+        fig = plt.figure(figsize=figsize)
+        ax = fig.add_subplot(111, projection='3d')
 
-        # 使用 text2D 将文字固定在 Axes 坐标系中
-        # x=0.5 表示水平居中
-        # y=-0.1 表示放在轴底端向下偏移的位置（根据需要可调整为 -0.15 或 -0.2）
-        axs[i].text2D(0.5, 0.1, caption_text,
-                      transform=axs[i].transAxes,
-                      horizontalalignment='center',
-                      verticalalignment='top',
-                      fontsize=15,
-                      fontname="Times New Roman")
+        # 绘制点云
+        sc = ax.scatter(X, Y, Z, c=values, s=point_size, vmin=vmin, vmax=vmax)
 
+        # 隐藏坐标轴
+        ax.set_axis_off()
 
-        # title setting
-        axs[i].title.set_fontsize(12)
-        axs[i].title.set_fontname("Times New Roman")
+        # 保持长宽高比例
+        eps = 1e-8
+        ax.set_box_aspect((max(x_range, eps), max(y_range, eps), max(z_range, eps)))
+        ax.view_init(elev=elev, azim=azim)
 
-        # axis label setting
-        axs[i].xaxis.label.set_fontsize(12)
-        axs[i].xaxis.label.set_fontname("Times New Roman")
-        axs[i].yaxis.label.set_fontsize(12)
-        axs[i].yaxis.label.set_fontname("Times New Roman")
-        axs[i].zaxis.label.set_fontsize(12)
-        axs[i].zaxis.label.set_fontname("Times New Roman")
+        # 设置点云显示范围，让点云尽量填满图片
+        margin = 0.02
+        ax.set_xlim(X.min() - x_range*margin, X.max() + x_range*margin)
+        ax.set_ylim(Y.min() - y_range*margin, Y.max() + y_range*margin)
+        ax.set_zlim(Z.min() - z_range*margin, Z.max() + z_range*margin)
 
-        # tick label setting
-        for tick in axs[i].get_xticklabels():
-            tick.set_fontsize(12)
-            tick.set_fontname("Times New Roman")
-        for tick in axs[i].get_yticklabels():
-            tick.set_fontsize(12)
-            tick.set_fontname("Times New Roman")
-        for tick in axs[i].get_zticklabels():
-            tick.set_fontsize(12)
-            tick.set_fontname("Times New Roman")
+        # colorbar 紧贴图像下方，长度与图像宽度对齐
+        cbar = fig.colorbar(sc, ax=ax, orientation='horizontal', pad=colorbar_pad, fraction=0.05)
+        cbar.ax.tick_params(labelsize=colorbar_fontsize)
 
-        # tick value setting
-        axs[i].xaxis.set_major_locator(plt.MaxNLocator(4))
-        axs[i].yaxis.set_major_locator(plt.MaxNLocator(4))
-        axs[i].zaxis.set_major_locator(plt.MaxNLocator(4))
+        plt.savefig(filename, dpi=300, bbox_inches='tight', pad_inches=0.01)
+        plt.close(fig)
 
-        # tick line setting
-        axs[i].tick_params(width=0.5, pad=2)
+    draw(y_true, f"{save_path}_true.png", vmin, vmax)
+    draw(y_pred, f"{save_path}_pred.png", vmin, vmax)
+    draw(abs_err, f"{save_path}_abs_error.png")
 
-        # Set box aspect based one actual data
-        data_x_range = x.max() - x.min()
-        data_y_range = y.max() - y.min()
-        data_z_range = z.max() - z.min()
-        axs[i].set_box_aspect((float(data_x_range), float(data_y_range), float(data_z_range)))
-
-    # ---- 统一 colorbar 尺寸 ----
-
-    cbar_ax01 = fig.add_subplot(gs[1, :2])   # 占据前两列的下方空间
-    cbar_ax2 = fig.add_subplot(gs[1, 2])   # 占据前两列的下方空间
-    fig.colorbar(scatters[0], cax=cbar_ax01, orientation='horizontal')
-    fig.colorbar(scatters[2], cax=cbar_ax2, orientation='horizontal')
-
-    if save_path is not None:
-        plt.savefig(save_path, dpi=300, bbox_inches='tight')
-        print(f" Figure saved to {save_path}")
-
-    plt.show()
-
+    print("Saved paper-style point cloud images with tight colorbar.")
