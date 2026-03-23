9fbc23d9276934c8ed7f8e9b059aadd1844e7f92
mae-zhangbj <1002129979@qq.com>
2026-03-24 04:36:26 +0800

    RopeTransolver/submit.sh Update

--- RopeTransolver/submit.sh
+++ RopeTransolver/submit.sh
@@ -11,7 +11,7 @@
 # This time
 # cuda:5
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=3
 
 # 确保 logs 目录存在
 LOG_DIR=./logs
@@ -22,8 +22,8 @@ TIMESTAMP=$(date "+%Y_%m%d_%H%M")
 
 # 启动训练（后台）
 # DrivAerML
-#nohup python train_DrivAerML.py config_DrivAerML_velocity.yml > ${LOG_DIR}/DrivAerML_velocity_${TIMESTAMP}.log 2>&1 &
-nohup python train_DrivAerML.py config_DrivAerML_spressure.yml > ${LOG_DIR}/DrivAerML_spressure_${TIMESTAMP}.log 2>&1 &
+nohup python train_DrivAerML.py config_DrivAerML_velocity.yml > ${LOG_DIR}/DrivAerML_velocity_${TIMESTAMP}.log 2>&1 &
+#nohup python train_DrivAerML.py config_DrivAerML_spressure.yml > ${LOG_DIR}/DrivAerML_spressure_${TIMESTAMP}.log 2>&1 &
 #nohup python train_DrivAerML.py config_DrivAerML_swss.yml > ${LOG_DIR}/DrivAerML_swss_${TIMESTAMP}.log 2>&1 &
 #nohup python test_model.py config_DrivAerML_velocity.yml > ${LOG_DIR}/DrivAerML_test_${TIMESTAMP}.log 2>&1 &
 
